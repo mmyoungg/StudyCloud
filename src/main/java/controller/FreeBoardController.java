9a238372e4d6e99145905faee126fdc31020f4f0
mmyoungg <die199074@gmail.com>
2022-12-02 19:10:35 +0900

    221202 자유게시판 Controller 수정 - 이민영

--- src/main/java/controller/FreeBoardController.java
+++ src/main/java/controller/FreeBoardController.java
@@ -19,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.google.gson.Gson;
-
 import dto.FileUpload;
 import dto.FreeBoard;
 import service.face.FreeBoardService;
